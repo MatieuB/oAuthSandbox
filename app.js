cc6ca05c340af32404f4d9a412b5995a2826c3f3
Matthew Bouchard <matthew@unself.com>
2017-09-21 10:15:25 -0600

    successful redirect

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport')
 var FacebookStrategy = require('passport-facebook')
+var session = require('express-session')
 require('dotenv').config()
 
 var index = require('./routes/index');
@@ -25,43 +26,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize())
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUnitialized: true
+}))
 app.use(passport.session())
 
 app.use('/', index)
 app.use('/users', users)
+
 passport.serializeUser(function(user, done) {
+  console.log('serialize');
   done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
-  done(null, user)
-});
-passport.use(new FacebookStrategy({
-    clientID: process.env.fbId,
-    clientSecret: process.env.fbSecret,
-    callbackURL: "http://localhost:3000/auth/facebook/callback",
-    scope: ['email', 'public_profile'],
-    profileFields: ['email', 'gender']
-  },
-  (accessToken, refreshToken, profile, cb) => {
-    console.log({accessToken,refreshToken,profile,cb})
-    User.findOrCreate({ facebookId: profile.id , profile: profile}, (err, user) => {
-      console.log('***** user ******', user);
-      return cb(null, user)
-    })
-  }))
-
+passport.deserializeUser(function(obj, done) {
+  console.log('deserializing ***************************************************',obj)
+  done(null, obj)
+})
 
 var User = {
   findOrCreate: function({facebookId, profile}){
     return new Promise((resolve, reject) => {
       if(facebookId) {
-        console.log('******************* successs **************************************************', profile)
-        resolve(profile)
+        console.log('******************* resolving ****************', profile)
+        resolve(facebookId)
       } else {
-        console.log('******************* rejected **************************************************')
+        console.log('******************* rejected ******************')
         const error = {'rejected': 'error'}
-        reject(error)
+        resolve(error)
       }
     })
   },
@@ -71,25 +65,56 @@ var User = {
         console.log('deserializeUser',id);
         resolve(true)
       } else {
+        console.log('rejected');
         reject({name:'deserializeUser error'})
       }
     })
   }
 }
+
+const fbOptions = {
+    clientID: process.env.fbId,
+    clientSecret: process.env.fbSecret,
+    callbackURL: "http://localhost:3000/auth/facebook/callback",
+    scope: ['email', 'public_profile'],
+    profileFields: ['email', 'gender', 'displayName'],
+    passReqToCallback: true
+  }
+
+function fbCallback(req, accessToken, refreshToken, profile, cb) {
+  console.log({req: req.session, accessToken, refreshToken, profile, cb})
+  return cb(null, profile)
+  User.findOrCreate({ facebookId: profile.id , profile: profile}, (err, user) => {
+    console.log('soemthing happned after findOrCreate!!!!!~!!!!!!!!!');
+    if(err) {
+      console.log('there was an error', err);
+    } else {
+      console.log('***** user ******', user);
+      return cb(null, profile)
+    }
+})
+}
+passport.use(new FacebookStrategy(fbOptions, fbCallback))
+
 app.get('/auth/facebook',
-  passport.authenticate('facebook'),(req, res)=> {
-    console.log('hit first auth')
-  });
+  passport.authenticate('facebook',{scope: ['email', 'public_profile'],session: true
+}))
 
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook',{scope: ['email', 'public_profile']}, { failureRedirect: '/' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    console.log('**************** success in /callback ***********************');
+app.get('/auth/facebook/callback',(req, res, next) =>  {
+  passport.authenticate('facebook',{
+    scope: ['email', 'public_profile'],
+    session: true,
+    failureRedirect: '/',
+    failureFlash:true },
+    (err, user, info) =>  {
+      console.log('user', user, 'info',info);
+      // Successful authentication, redirect home.
+      console.log('**************** success in /callback ***********************')
 
-    res.redirect('/success');
-  });
+    res.redirect('/success')
+  })(req, res, next)
 
+})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
